cbafe00ed67b898e6a861cbb4ede429c65e839cb
Lee Li <linjielig@gmail.com>
2023-07-16 21:49:50 +0500

    create blog page

---
+++ app/blog/[slug]/page.js
@@ -0,0 +1,19 @@
+import { getPostData } from '../../../lib/posts'
+import Head from 'next/head'
+
+export default async function Page({ params }) {
+  const data = await getData(params.slug)
+  return (
+    <artile>
+      <h1>{data.title}</h1>
+      <div>{data.date}</div>
+      <div dangerouslySetInnerHTML={{ __html: data.contentHtml }} />
+    </artile>
+  )
+}
+
+async function getData(slug) {
+  const data = await getPostData(slug)
+
+  return data;
+}
